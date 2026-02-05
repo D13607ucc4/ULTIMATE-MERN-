4d54a8e70377e72a462faa4b726205a0abca2fc0
Diego Zucca <diego.zucca@correo.ucu.edu.uy>
2026-02-05 11:10:21 -0300

    First commit

---
+++ src/controllers/auth.controllers.js
@@ -0,0 +1,18 @@
+import User from '../models/user.model.js'
+
+export const register = (req, res) => {
+    const { username, email, password } = req.body
+
+    const newUser = new User({
+        username, 
+        email, 
+        password
+    })
+
+    console.log(newUser)
+
+    res.send('registrando...')
+    
+}
+
+export const login = (req, res) => res.send('login')
