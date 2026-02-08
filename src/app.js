b41e28724513aec90e88903c8379bf53d8e92c45
Diego Zucca <diego.zucca@correo.ucu.edu.uy>
2026-02-08 17:11:35 -0300

    feat: add user registration and login pages, integrate axios for API requests

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express, { json } from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
